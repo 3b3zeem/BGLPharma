0d44b32acc9938b93d3303133408f038aa174544
3b3zeem <ahmedzezo2247@gmail.com>
2025-07-15 00:27:34 +0300

    Add About component and integrate it into Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 // import { useLanguage } from "../../context/Language/LanguageContext";
 import Meta from "../../components/Meta/Meta";
 import Hero from "../../components/HomeComponents/Hero/Hero";
+import About from "../../components/HomeComponents/About/About";
 
 const Home = () => {
   // const { language } = useLanguage();
@@ -16,6 +17,7 @@ const Home = () => {
           description="Welcome to BGL Pharma, your trusted source for pharmaceutical products."
         />
         <Hero />
+        <About />
       </div>
     </React.Fragment>
   );
