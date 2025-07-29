1d08ad4bc2239d10f9e38b45aa5d37677fad5292
3b3zeem <ahmedzezo2247@gmail.com>
2025-07-29 14:40:10 +0300

    Change the api url

--- src/Hooks/useContact.jsx
+++ src/Hooks/useContact.jsx
@@ -10,13 +10,16 @@ const useContact = () => {
     setError(null);
 
     try {
-      const response = await fetch("http://localhost:5000/api/BGLContact", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ name, email, message }),
-      });
+      const response = await fetch(
+        "https://bgl-api-contact.vercel.app/api/BGLContact",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ name, email, message }),
+        }
+      );
 
       if (!response.ok) {
         throw new Error("Failed to send message");
